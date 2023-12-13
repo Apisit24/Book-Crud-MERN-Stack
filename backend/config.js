77a3ca028003ff4d318db4abc4d131d021c07a3f
Apisit <apisit.mudsang@gmail.com>
2023-12-13 18:05:07 +0700

    CRUD Books V1.0

---
+++ backend/config.js
@@ -0,0 +1,6 @@
+export const PORT = 3000;
+
+export const mongoDBURL = "mongodb://localhost:27017/book";
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
